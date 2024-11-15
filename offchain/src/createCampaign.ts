3d3c2151fd12783412a08b3c131ac6d9133b9aa1
Ariady Putra <2069784+ariady-putra@users.noreply.github.com>
2024-11-15 19:31:29 +0700

    During pledgeCampaign you must send back the datum

--- offchain/src/createCampaign.ts
+++ offchain/src/createCampaign.ts
@@ -1,33 +1,27 @@
-import { Data, Emulator, EmulatorAccount, LucidEvolution, paymentCredentialOf, validatorToAddress } from "@lucid-evolution/lucid"
+import { Data, Emulator, EmulatorAccount, LucidEvolution, paymentCredentialOf, validatorToAddress } from "@lucid-evolution/lucid";
 import { CFDatum } from "./types";
 import { spendingValidator } from "./validators";
 
-
 export async function createCampaign(account: EmulatorAccount, lucid: LucidEvolution, emulator: Emulator) {
-    const contractAddress = validatorToAddress(lucid.config().network, spendingValidator);
-    lucid.selectWallet.fromSeed(account.seedPhrase)
-    const datum: CFDatum = {
-        campaign_id:"666f6f",
-        title: "666f6f",
-        goal: BigInt(100),
-        creator: paymentCredentialOf(account.address).hash,
-        deadline: BigInt(Date.now() + 777539000)
-    }
+  const contractAddress = validatorToAddress(lucid.config().network, spendingValidator);
+  lucid.selectWallet.fromSeed(account.seedPhrase);
+  const datum: CFDatum = {
+    campaign_id: "666f6f",
+    title: "666f6f",
+    goal: BigInt(100),
+    creator: paymentCredentialOf(account.address).hash,
+    deadline: BigInt(Date.now() + 777539000),
+  };
 
-    const tx = await lucid
-        .newTx()
-        .pay.ToAddressWithData(
-            contractAddress,
-            { kind: "inline", value: Data.to(datum, CFDatum) },
-            { lovelace: 5_000_000n }
-        )
-        .complete();
+  const tx = await lucid
+    .newTx()
+    .pay.ToAddressWithData(contractAddress, { kind: "inline", value: Data.to(datum, CFDatum) }, { lovelace: 5_000_000n })
+    .complete();
 
-    const signedTx = await tx.sign.withWallet().complete();
-    const txHash = await signedTx.submit();
+  const signedTx = await tx.sign.withWallet().complete();
+  const txHash = await signedTx.submit();
 
-    emulator.awaitTx(txHash)
-    const allUTxOs = await lucid.utxosAt(contractAddress);
-    return allUTxOs
+  emulator.awaitTx(txHash);
+  const allUTxOs = await lucid.utxosAt(contractAddress);
+  return allUTxOs;
 }
-
--- offchain/src/index.ts
+++ offchain/src/index.ts
@@ -2,31 +2,26 @@ import { Emulator, generateEmulatorAccount, Lucid, Parameters, paymentCredential
 import { createCampaign } from "./createCampaign";
 import { pledgeCampaign } from "./pledgeCampaign";
 
-export const creator = generateEmulatorAccount({ lovelace: 12_000_000_000n })
-export const alice = generateEmulatorAccount({ lovelace: 12_000_000_000n })
-export const bob = generateEmulatorAccount({ lovelace: 12_000_000_000n })
-export const charlie = generateEmulatorAccount({ lovelace: 12_000_000_000n })
+export const creator = generateEmulatorAccount({ lovelace: 12_000_000_000n });
+export const alice = generateEmulatorAccount({ lovelace: 12_000_000_000n });
+export const bob = generateEmulatorAccount({ lovelace: 12_000_000_000n });
+export const charlie = generateEmulatorAccount({ lovelace: 12_000_000_000n });
 
-const emulator = new Emulator(
-    [creator, alice, bob, charlie]
-)
+const emulator = new Emulator([creator, alice, bob, charlie]);
 
 export async function startContractExe() {
-    console.clear()
-    console.log("-------------------------Start Emulation-------------------------")
-    console.log("creator", creator.address)
+  console.clear();
+  console.log("-------------------------Start Emulation-------------------------");
+  console.log("creator", creator.address);
 
-    const lucid = await Lucid(emulator, "Custom")
+  const lucid = await Lucid(emulator, "Custom");
 
+  emulator.awaitBlock(2); //wait for 2 block
 
-    emulator.awaitBlock(2) //wait for 2 block
+  const campaignUTxO = await createCampaign(creator, lucid, emulator);
+  console.log("-------------------------Campaign Created-------------------------");
 
-    const campaignUTxO = await createCampaign(creator, lucid, emulator)
-console.log("-------------------------Campaign Created-------------------------")
-
-
-    await pledgeCampaign(alice, campaignUTxO, lucid, emulator)
-
+  await pledgeCampaign(alice, campaignUTxO, lucid, emulator);
 }
 
-await startContractExe()
+await startContractExe();
