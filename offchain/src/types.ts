5f4552fd434136c85ffde64efe1ec037ca62d9db
Ariady Putra <2069784+ariady-putra@users.noreply.github.com>
2024-11-15 19:31:59 +0700

    Just need to collect output_utxo once

--- offchain/src/types.ts
+++ offchain/src/types.ts
@@ -1,12 +1,11 @@
-import { Data } from "@lucid-evolution/lucid"
-
+import { Data } from "@lucid-evolution/lucid";
 
 const DatumSchema = Data.Object({
-    campaign_id: Data.Bytes(),
-    title: Data.Bytes(),
-    creator: Data.Bytes(),
-    deadline: Data.Integer(),
-    goal: Data.Integer(),
+  campaign_id: Data.Bytes(),
+  title: Data.Bytes(),
+  creator: Data.Bytes(),
+  deadline: Data.Integer(),
+  goal: Data.Integer(),
 });
 export type CFDatum = Data.Static<typeof DatumSchema>;
 export const CFDatum = DatumSchema as unknown as CFDatum;
